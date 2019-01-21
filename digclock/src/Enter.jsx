f944f299e5a20228ed8cdbcd3240cc0041fd1eb4
karen-edith <karen.edith.garcia1@gmail.com>
2019-01-21 14:08:48 -0800

    section styling changes

--- digclock/src/Enter.jsx
+++ digclock/src/Enter.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './Enter.css';
-import { Button } from 'react-bootstrap';
+import { Button, Glyphicon } from 'react-bootstrap';
 
 class Enter extends Component {
 
@@ -8,21 +8,21 @@ class Enter extends Component {
     return(
       <div className = 'overallContainer'>
         <div className = 'expectations'>
-          <h4> Entering Room Expectations </h4>
-          <div className = 'rule'> Line up outside in a straight line - track forward with voice at a level 1 </div>
-          <div className = 'rule'> Take out homework, book, or grab art/crafts supplies from bin </div>
-          <div className = 'rule'> Put Backpack away on Rack </div>
-          <div className = 'rule'> Have a seat and work on selected option and keep voice at a level 1 </div>
-          <div className = 'rule'> For announcements, track Ms. Garcia when she requests your attention with an attention getter </div>
-          <div className = 'rule'> Once You're done with your Job, Let Ms. Garcia know and have a seat </div>
-          <div className = 'rule'> Everyon one else should have a seat </div>
+          <div className = 'expectationsTitle'> Entering Room Expectations </div>
+          <div className = 'rule'><div className = 'handglyph'><Glyphicon glyph = 'hand-right'/></div> <div className ='rle'>Line up outside in a straight line - track forward with voice at a level 1 </div></div>
+          <div className = 'rule'><div className = 'handglyph'><Glyphicon glyph = 'hand-right'/></div> <div className ='rle'>Take out homework, book, or grab art/crafts supplies from bin </div></div>
+          <div className = 'rule'><div className = 'handglyph'><Glyphicon glyph = 'hand-right'/></div> <div className ='rle'>Put Backpack away on Rack </div></div>
+          <div className = 'rule'><div className = 'handglyph'><Glyphicon glyph = 'hand-right'/></div> <div className ='rle'>Have a seat and work on selected option and keep voice at a level 1 </div></div>
+          <div className = 'rule'><div className = 'handglyph'><Glyphicon glyph = 'hand-right'/></div> <div className ='rle'>For announcements, track Ms. Garcia when she requests your attention with an attention getter </div></div>
+          <div className = 'rule'><div className = 'handglyph'><Glyphicon glyph = 'hand-right'/></div> <div className ='rle'>Once You're done with your Job, Let Ms. Garcia know and have a seat </div></div>
+          <div className = 'rule'><div className = 'handglyph'><Glyphicon glyph = 'hand-right'/></div> <div className ='rle'>Everyon one else should have a seat </div></div>
           <div>
             <Button> Back to </Button>
             <Button> Today's Activities </Button>
           </div>
         </div>
         <div className = 'jobs'>
-          <h4> Student Jobs</h4>
+          <div className = 'jobsTitle'> Block Student Jobs </div>
           <div className = 'jobrow'>
             <div className ='titl'> <div className = 'jtitle1'> Line Leader </div> <div className='jtitle2'> Attendance Assistant</div></div>
             <div className ='studentImage'> <img className = 'image redborder' src = {require('./sbob.jpg')} alt =''/> <div className ='studentName'> Nellie</div> </div>
@@ -30,19 +30,19 @@ class Enter extends Component {
           </div>
 
           <div className = 'jobrow'>
-            <div className ='title'> <div className = 'jtitle1'> Sign-in Manager </div> <div className='jtitle2'> Laptop Setup Assistant </div></div>
+            <div className ='titl'> <div className = 'jtitle1'> Sign-in Manager </div> <div className='jtitle2'> Laptop Setup Assistant </div></div>
             <div className ='studentImage'> <img className = 'image greenborder' src = {require('./sbob.jpg')} alt =''/> <div className ='studentName'> Joshua </div> </div>
             <div className ='studentImage'> <img className = 'image greenborder' src = {require('./sbob.jpg')} alt =''/> <div className ='studentName'> Alyssa </div> </div>
           </div>
 
           <div className = 'jobrow'>
-            <div className ='title'> <div className = 'jtitle'> Backpack Managers </div> </div>
+            <div className ='titl'> <div className = 'jtitle'> Backpack Managers </div> </div>
             <div className ='studentImage'> <img className = 'image blueborder' src = {require('./sbob.jpg')} alt =''/> <div className ='studentName'> Reizo </div> </div>
             <div className ='studentImage'> <img className = 'image blueborder' src = {require('./sbob.jpg')} alt =''/> <div className ='studentName'> Dominic </div> </div>
           </div>
 
-          <div className = 'jobrow'>
-            <div className ='title'> <div className = 'jtitle1'> Projector Assitant </div> <div className='jtitle2'> Timer Assistant </div></div>
+          <div className = 'jobrow nobottomborder'>
+            <div className ='titl'> <div className = 'jtitle1'> Projector Assitant </div> <div className='jtitle2'> Timer Assistant </div></div>
             <div className ='studentImage'> <img className = 'image redborder' src = {require('./sbob.jpg')} alt =''/> <div className ='studentName'> Julio </div> </div>
             <div className ='studentImage'> <img className = 'image redborder' src = {require('./sbob.jpg')} alt =''/> <div className ='studentName'> Allison </div> </div>
           </div>
