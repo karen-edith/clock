03185464c396f0d48983fc43dcfd291d6772d1b5
karen-edith <karen.edith.garcia1@gmail.com>
2019-01-21 14:28:51 -0800

    button styling + added button links

--- digclock/src/Enter.jsx
+++ digclock/src/Enter.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './Enter.css';
 import { Button, Glyphicon } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 class Enter extends Component {
 
@@ -16,35 +17,35 @@ class Enter extends Component {
           <div className = 'rule'><div className = 'handglyph'><Glyphicon glyph = 'hand-right'/></div> <div className ='rle'>For announcements, track Ms. Garcia when she requests your attention with an attention getter </div></div>
           <div className = 'rule'><div className = 'handglyph'><Glyphicon glyph = 'hand-right'/></div> <div className ='rle'>Once You're done with your Job, Let Ms. Garcia know and have a seat </div></div>
           <div className = 'rule'><div className = 'handglyph'><Glyphicon glyph = 'hand-right'/></div> <div className ='rle'>Everyon one else should have a seat </div></div>
-          <div>
-            <Button> Back to </Button>
-            <Button> Today's Activities </Button>
+          <div className ='navigation'>
+            <div> <Button className = 'backtobtn'> <Link to='/' className='removelinkdec'> Clock and Schedule </Link> </Button> </div>
+            <div><Button className='todaysactivities'> Today's Activities </Button></div>
           </div>
         </div>
         <div className = 'jobs'>
           <div className = 'jobsTitle'> Block Student Jobs </div>
           <div className = 'jobrow'>
             <div className ='titl'> <div className = 'jtitle1'> Line Leader </div> <div className='jtitle2'> Attendance Assistant</div></div>
-            <div className ='studentImage'> <img className = 'image redborder' src = {require('./sbob.jpg')} alt =''/> <div className ='studentName'> Nellie</div> </div>
-            <div className ='studentImage'> <img className = 'image redborder' src = {require('./sbob.jpg')} alt =''/> <div className ='studentName'> Jeihser </div> </div>
+            <div className ='studentImage'> <img className = 'image' src = {require('./sbob.jpg')} alt =''/> <div className ='studentName'> Nellie</div> </div>
+            <div className ='studentImage'> <img className = 'image' src = {require('./sbob.jpg')} alt =''/> <div className ='studentName'> Jeihser </div> </div>
           </div>
 
           <div className = 'jobrow'>
             <div className ='titl'> <div className = 'jtitle1'> Sign-in Manager </div> <div className='jtitle2'> Laptop Setup Assistant </div></div>
-            <div className ='studentImage'> <img className = 'image greenborder' src = {require('./sbob.jpg')} alt =''/> <div className ='studentName'> Joshua </div> </div>
-            <div className ='studentImage'> <img className = 'image greenborder' src = {require('./sbob.jpg')} alt =''/> <div className ='studentName'> Alyssa </div> </div>
+            <div className ='studentImage'> <img className = 'image' src = {require('./sbob.jpg')} alt =''/> <div className ='studentName'> Joshua </div> </div>
+            <div className ='studentImage'> <img className = 'image' src = {require('./sbob.jpg')} alt =''/> <div className ='studentName'> Alyssa </div> </div>
           </div>
 
           <div className = 'jobrow'>
             <div className ='titl'> <div className = 'jtitle'> Backpack Managers </div> </div>
-            <div className ='studentImage'> <img className = 'image blueborder' src = {require('./sbob.jpg')} alt =''/> <div className ='studentName'> Reizo </div> </div>
-            <div className ='studentImage'> <img className = 'image blueborder' src = {require('./sbob.jpg')} alt =''/> <div className ='studentName'> Dominic </div> </div>
+            <div className ='studentImage'> <img className = 'image' src = {require('./sbob.jpg')} alt =''/> <div className ='studentName'> Reizo </div> </div>
+            <div className ='studentImage'> <img className = 'image' src = {require('./sbob.jpg')} alt =''/> <div className ='studentName'> Dominic </div> </div>
           </div>
 
           <div className = 'jobrow nobottomborder'>
             <div className ='titl'> <div className = 'jtitle1'> Projector Assitant </div> <div className='jtitle2'> Timer Assistant </div></div>
-            <div className ='studentImage'> <img className = 'image redborder' src = {require('./sbob.jpg')} alt =''/> <div className ='studentName'> Julio </div> </div>
-            <div className ='studentImage'> <img className = 'image redborder' src = {require('./sbob.jpg')} alt =''/> <div className ='studentName'> Allison </div> </div>
+            <div className ='studentImage'> <img className = 'image' src = {require('./sbob.jpg')} alt =''/> <div className ='studentName'> Julio </div> </div>
+            <div className ='studentImage'> <img className = 'image' src = {require('./sbob.jpg')} alt =''/> <div className ='studentName'> Allison </div> </div>
           </div>
 
         </div>
