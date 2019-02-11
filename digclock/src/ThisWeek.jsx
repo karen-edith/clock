ebd7414563e82f35f09a0cba9b9ceae89c3554d9
karen-edith <karen.edith.garcia1@gmail.com>
2019-02-11 14:04:40 -0800

    outlined and did basic styling for activities page

--- digclock/src/ThisWeek.jsx
+++ digclock/src/ThisWeek.jsx
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import './ThisWeek.css';
 
 class ThisWeek extends Component {
   render() {
     return (
       <div>
-      Activities
+        <div className='Sports'> Sports </div>
+        <div className='Ranking'> Homework Ranking </div>
+        <div className='Loud'> Loud </div>
+        <div className='Dance'> Dance </div>
+        <div className='Announce'> Announcements </div>
+        <div className='Trips'> Field Trips </div>
       </div>
     )
   }
