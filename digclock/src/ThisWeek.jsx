04d719c92db8a47b172b8c53135d6a0a98d8c85f
karen-edith <karen.edith.garcia1@gmail.com>
2019-02-11 22:47:31 -0800

    styling actvities sections

--- digclock/src/ThisWeek.jsx
+++ digclock/src/ThisWeek.jsx
@@ -5,12 +5,12 @@ class ThisWeek extends Component {
   render() {
     return (
       <div className='activitiesContainer'>
-        <div className='Sports'> Sports </div>
-        <div className='Ranking'> Homework Ranking </div>
-        <div className='Loud'> Loud </div>
-        <div className='Dance'> Dance </div>
-        <div className='Announce'> Announcements </div>
-        <div className='Trips'> Field Trips </div>
+        <div className ='outer'> <div className='Sports'> <div className='inner'> Sports </div> </div> </div>
+        <div className ='outer'> <div className='Ranking'> <div className='inner'> Homework Ranking </div> </div> </div>
+        <div className ='outer'> <div className='Loud'> <div className='inner'> Loud </div> </div> </div>
+        <div className ='outer'> <div className='Dance'> <div className='inner'> Dance </div> </div> </div>
+        <div className ='outer'> <div className='Announce'> <div className='inner'> Announcements </div> </div> </div>
+        <div className ='outer'> <div className='Trips'> <div className='inner'> Field Trips </div> </div> </div>
       </div>
     )
   }
